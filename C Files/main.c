11d505181c0cea244e8fc4dba9a5e0b99cb01221
CankutBoraTuncer <77197110+CankutBoraTuncer@users.noreply.github.com>
2022-06-18 20:37:42 +0300

    Update main.c

--- C Files/main.c
+++ C Files/main.c
@@ -1,16 +1,16 @@
 #include "project_header.h"
 
 int main(void){	
-	WDT_init();
-	Systick_init();
-	__enable_irq();
-	PWM_init();
-	motor_init();
-	UART_init();
+	WDT_init();                                        // Watchdog init
+	Systick_init();                                    // Systick init
+	__enable_irq();                                    // Enable interrupts
+	PWM_init();                                        // PWM init
+	Motor_init();                                      // Motor init
+	UART_init();                                       // UART init
 	while (1){	
-		Service_COP_WDT();
+		Service_COP_WDT();                             // Service the watchdog
 	}	
-}	
+}
 
 
 
